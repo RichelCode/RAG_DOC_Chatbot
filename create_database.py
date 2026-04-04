c408cea6acc4556c1be525509a2efcf25bb0a1da
Richel Attafuah <richelattafuah@gmail.com>
2026-04-03 21:51:52 -0400

    Add data files and update project

--- create_database.py
+++ create_database.py
@@ -1,16 +1,26 @@
-import os
-import shutil
-from dotenv import load_dotenv
-
-from langchain_community.document_loaders import DirectoryLoader, TextLoader
+# from langchain.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+#from langchain.prompts import ChatPromptTemplacument
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain.embeddings import OpenAIEmbeddings
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
+import openai 
+from dotenv import load_dotenv
+import os
+import shutil
 
+# Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
+#---- Set OpenAI API key 
+# Change environment variable name from "OPENAI_API_KEY" to the name given in 
+# your .env file.
+openai.api_key = os.environ['OPENAI_API_KEY']
 
-DATA_PATH = "data/books"
 CHROMA_PATH = "chroma"
+DATA_PATH = "data/books"
 
 
 def main():
@@ -24,36 +34,36 @@ def generate_data_store():
 
 
 def load_documents():
-    loader = DirectoryLoader(
-        DATA_PATH,
-        glob="*.md",
-        loader_cls=TextLoader,
-        loader_kwargs={"encoding": "utf-8"}
-    )
+    loader = DirectoryLoader(DATA_PATH, glob="*.md")
     documents = loader.load()
-    print(f"Loaded {len(documents)} documents.")
     return documents
 
 
-def split_text(documents):
+def split_text(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
     )
     chunks = text_splitter.split_documents(documents)
-    print(f"Split into {len(chunks)} chunks.")
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    document = chunks[10]
+    print(document.page_content)
+    print(document.metadata)
+
     return chunks
 
 
-def save_to_chroma(chunks):
+def save_to_chroma(chunks: list[Document]):
+    # Clear out the database first.
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
+    # Create a new DB from the documents.
     db = Chroma.from_documents(
-        chunks,
-        OpenAIEmbeddings(),
-        persist_directory=CHROMA_PATH
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
     )
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
